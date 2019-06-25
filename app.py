9fc65fa5900cfa839138408732f357b87b8491e8
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-06-26 01:55:24 +0900

    Add initialization of colorama

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from modules.InitProcess import InitProcess
+import colorama
 
 def main():
     # Initial Process
+    colorama.init() # Initialization of colorama for StrFormat.py
     init = InitProcess()
     os, mode, uuid = init.get_init_parameters()
     print("OS: {}".format(os))
+    print("mode: {}".format(mode))
     print("Your ID is: {}".format(uuid))
 
     # Start main process in accordance with mode
