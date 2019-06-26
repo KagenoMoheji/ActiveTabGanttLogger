ce163a8d789692e6c089b966909209cfe689cbda
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-06-26 17:52:51 +0900

    Add test guide and some functions

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from modules.InitProcess import InitProcess
 from modules.StrFormatter import StrFormatter
+from modules.Observer import Observer
+from modules.Logger import Logger
+
+'''
+[How to test]
+If you have setted the pipenv, start from (3).
+Or, if you have installed this CLI, start from (4).
+(1)Install pipenv.
+`python -m pip install pipenv` or `pip3 install pipenv`
+(2)Create virtual python environment with pipenv.
+`pipenv --python 3.7`
+(3)Install this CLI.
+`pipenv run python -m pip install -e .`
+(4)Run CLI.
+`pipenv run ganttlogger`
+'''
 
 def main():
     '''
@@ -11,13 +27,39 @@ def main():
     # Main initialization
     init = InitProcess()
     os, mode, uuid = init.get_init_parameters()
+    # print("OS: {}".format(os)) # "w" or "d"
 
-    print("OS: {}".format(os))
+    # Start main process(thread-loop) in accordance with mode
+    if mode is "Alone":
+        alone(os)
+    elif mode is "Observer":
+        observer(os, uuid)
+    elif mode is "Logger":
+        logger(os, uuid)
+    '''
+    elif mode is "Plotter":
+        plotter(os)
+    '''
 
-    # Start main process in accordance with mode
+    # Exit the loop above
+    # Close process?
 
-    # Close process
+def alone(os):
+    print("This mode is Alone")
+    pass
 
+def observer(os, uuid):
+    print("This mode is Observer")
+    pass
+
+def logger(os, uuid):
+    print("This mode is Logger")
+    pass
+
+'''
+def plotter():
+    pass
+'''
 
 if __name__ is "__main__":
     main()
