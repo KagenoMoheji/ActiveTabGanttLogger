c23a13a928ba1f3f15b2e01ef277acd206256d23
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-06-27 17:45:46 +0900

    Tried tab-observer on Windows10

--- app.py
+++ app.py
@@ -64,14 +64,39 @@ if __name__ == "__main__":
     '''
     ここの処理を実行する`pipenv run python app.py`は，お試し実装の場とする．
     コメントも日本語OKで．
+
+    [メモ]
+    ●実際にobserverするとき，長時間稼働して配列(numpy)に格納するデータ数膨大に
+    なるから，一定時間(5-10m)経った時にそれまでの配列を別スレッド立ててファイル
+    書き出ししつつ元の配列の変数を初期化して再利用する並列処理した方がええのかな．
+
+    ●「デルタt間」のマウス移動ピクセル数・キーボード打鍵数はどう取得したものかな…
+    →アクティブタブに対して操作しているかは考えず，普通にマウス移動・キーボード打鍵
+    の検出をしていく？
+    まぁ最前面に表示するソフトを使うとかの悪者出てきそうだけど無い前提で研究すればいいか．
+    →開始時刻をあらかじめ変数に格納してデルタt経ったらマウス移動・キーボード打鍵の
+    総和を格納する変数を初期化？
+    →そもそもデルタt＝1sとか短めに設定しておいて(最低デルタtにして)データ取得して，
+    プロット時に最低デルタt以上の指定で自由な間隔調整ができるようにする？
+
+    ●拡張ディスプレイ・仮想デスクトップでもアクティブタブは認識できてる？
+    ・仮想デスクトップだと，デスクトップ別のアクティブタブが選択される状態になる．
+    →そこまで気にすることないか？
+    →短時間でタブ切り替えされたアプリケーションは，使われないものとして除去するのもアリ
+    ・拡張ディスプレイで，タイマー(ブラウザ)を映すことやってみるか
     '''
+    import platform
+    from datetime import datetime
+    import time
     # main()
 
-#     # Get PID and its name
-#     # https://githubja.com/giampaolo/psutil
-#     # https://psutil.readthedocs.io/en/latest/#process-class
-#     # psutilモジュールではアクティブタブは分からなさそう．
-#     # psutil: {Windows: OK, MacOS: OK}
+    '''
+    [Get PID and its name]
+    ・https://githubja.com/giampaolo/psutil
+    ・https://psutil.readthedocs.io/en/latest/#process-class
+    ●psutilモジュールではアクティブタブは分からなさそう．
+    ●psutil: {Windows: OK, MacOS: OK}
+    '''
     import psutil
 #     for ps in psutil.process_iter():
 #         text = """\
@@ -83,41 +108,74 @@ if __name__ == "__main__":
 # """.format(ppid=ps.ppid(), pid=ps.pid, name=ps.name(), status=ps.status())
 #         print(text)
 
-    # [Windows10-64bit]Get active window
-    # (wnck)https://askubuntu.com/a/483619 -> Failed. Only run on Unix?
-    # (linux向けらしい)https://stackoverflow.com/questions/46628209/get-the-process-of-the-active-window-with-python-in-linux
-    # (win32gui)https://www.reddit.com/r/learnpython/comments/90onta/getting_activeforeground_window_title_on_windows/
-    # win32guiのモジュールのインストールについて
-    # http://blog.livedoor.jp/kmiwa_project/archives/1058907748.html
-    # https://sourceforge.net/projects/pywin32/files/pywin32/Build%20221/
-    # https://github.com/mhammond/pywin32/releases
-    # https://stackoverflow.com/questions/42370339/python-3-6-install-win32api
-    # https://github.com/Googulator/pypiwin32
-    # Windows10-64bitに向けては，`pip install pypiwin32`を採用
-    import win32gui as w
-    import win32process as wp
-    from datetime import datetime
-    import time
-    # pids = wp.GetWindowThreadProcessId(w.GetForegroundWindow())
-    # print(psutil.Process(pids[-1]))
-    current_active_pid = -1
-    try:
-        while True:
-            # 同じアプリケーションで異なるウィンドウを開いていても同じpidで区別はつかないらしい
-            pid = wp.GetWindowThreadProcessId(w.GetForegroundWindow())[-1]
-            if pid is not current_active_pid:
-                current_active_pid = pid
-            print("{time}: {pid}: {active_name}".format(
-                time=datetime.now().strftime("%H:%M:%S.%f"),
-                pid=current_active_pid,
-                active_name=psutil.Process(current_active_pid).name()))
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("Exit")
-    # [MacOS]Get active window
-    # (Win: win32gui, Mac: Appkit)https://stackoverflow.com/a/36419702
-    # (Mac Appkit)https://codeday.me/jp/qa/20190523/885948.html
-    # (Mac向け？xpropやxdotoolのインストールが必要？)https://stackoverflow.com/questions/3983946/get-active-window-title-in-x
+    os = platform.platform(terse=True)
+    if "Windows" in os:
+        '''
+        [Get active window ※Windows10-64bit]
+        ・(win32gui)https://www.reddit.com/r/learnpython/comments/90onta/getting_activeforeground_window_title_on_windows/
+        ●win32guiのモジュールのインストールについて
+        ・http://blog.livedoor.jp/kmiwa_project/archives/1058907748.html
+        ・https://sourceforge.net/projects/pywin32/files/pywin32/Build%20221/
+        ・https://github.com/mhammond/pywin32/releases
+        ・https://stackoverflow.com/questions/42370339/python-3-6-install-win32api
+        ・https://github.com/Googulator/pypiwin32
+        →Windows10-64bitに向けては，`pip install pypiwin32`を採用
+        ●win32系のドキュメント？
+        ・http://docs.activestate.com/activepython/2.4/pywin32/win32_modules.html
+        ●アクティブタブがchromeだった場合のリンクの取得について
+        ・https://stackoverflow.com/questions/11645123/how-do-i-get-the-url-of-the-active-google-chrome-tab-in-windows
+        ・https://codeday.me/jp/qa/20190401/509668.html
+
+
+        '''
+        import win32gui as wg
+        import win32process as wp
+        import win32com.client as wcli
+        # import win32con as wc
+        # pids = wp.GetWindowThreadProcessId(w.GetForegroundWindow())
+        # print(psutil.Process(pids[-1]))
+
+        recent_active_pid = -1
+        try:
+            while True:
+                # ForegroundWindowのオブジェクト取得
+                fw = wg.GetForegroundWindow()
+                # pidの取得
+                # 同じアプリケーションで異なるウィンドウを開いていても同じpidで区別はつかないらしい
+                active_pid = wp.GetWindowThreadProcessId(fw)[-1]
+                if active_pid is not recent_active_pid:
+                    recent_active_pid = active_pid
+                # fwの実行ファイル名の取得
+                active_name = psutil.Process(recent_active_pid).name()
+                # fwの詳細テキストの取得
+                # 下記の一行でステータスバー(ブラウザならページのタイトル)のテキストを取得できる
+                tab_text = wg.GetWindowText(fw)
+                if "CHROME" in active_name.upper(): # Chromeなら
+                    tab_text_list = tab_text.split(" - ")[:-1]
+                    tab_text = " - ".join(tab_text_list)
+
+                # ブラウザならtab_textはURL，それ以外はステータスバー
+                print("{time}: {pid}: {active_name}({tab_text})".format(
+                    time=datetime.now().strftime("%H:%M:%S.%f"),
+                    pid=recent_active_pid,
+                    active_name=active_name,
+                    tab_text=tab_text))
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print("Exit")
+    elif "Darwin" in os:
+        '''
+        [Get active window ※MacOS Mojave]
+        ・(Win: win32gui, Mac: Appkit)https://stackoverflow.com/a/36419702
+        ・(Mac Appkit)https://codeday.me/jp/qa/20190523/885948.html
+        ・(Mac向け？xpropやxdotoolのインストールが必要？)https://stackoverflow.com/questions/3983946/get-active-window-title-in-x
+        ●アクティブタブがchromeだった場合のリンクの取得について
+
+
+        '''
+        print("MacOS")
+
+
     
 
 
