fa30c7dd394884a5f27ab58847c5dec4deef7330
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-06-27 19:56:13 +0900

    Add References

--- modules/Logger.py
+++ modules/Logger.py
@@ -1,13 +1,23 @@
 '''
+●jsonを引数に受け取る
 ●jsonのid順にログ追記
 ●生ログ出力
 ●ガントチャート生成・出力
 ・ファイル名にタイムスタンプつけとく
 '''
 import matplotlib.pyplot as plt
+import numpy as np
+# import threading
+import concurrent.futures as confu
+from datetime import datetime # 定期間隔書き出しのタイムスタンプ用
 
 class Logger:
-    def __init__(self):
+    os = ""
+    def __init__(self, os, uuid=""):
+        self.os = os
+        if uuid:
+            self.uuid = uuid
+        # データ受け取り・定期間隔でのファイル書き出しの並列処理なので最大2？
         pass
 
     def close(self): pass
