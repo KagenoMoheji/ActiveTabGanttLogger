063b0ac983306bbc370246e4a637f2917becc9b3
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-06-29 16:51:34 +0900

    Try getting cursor-move-distance on Mac

--- app.py
+++ app.py
@@ -95,22 +95,55 @@ if __name__ == "__main__":
     from datetime import datetime
     import time
     import psutil
+    import math
+    import pyautogui
 
     os = platform.platform(terse=True)
-    recent_active_tab_text = "START!"
     if "Windows" in os:
-        import win32gui as wg
-        import win32process as wp
-        import win32com.client as wcli
+        '''
+        [マウスの移動ピクセル数を1s間隔で検出する]
+        ●リアルタイムで動作確認するとき，出力されたログをVSCodeで開いて
+        続けてアクションを起こすと更新を見ることができそう.
+        ●[payautogui]
+        ・https://qiita.com/hirohiro77/items/78e26a59c2e45a0fe4e3
+        ・https://pyautogui.readthedocs.io/en/latest/mouse.html#the-screen-and-mouse-position
+        ●「1秒間隔」
+        ・unix時間の差分で求めるのが良さそう．
+        ・datetimeのmicrosecondsでやろうと思ったけど，1000000になる前に0にされるので差分計算できない
+        ・https://note.nkmk.me/python-datetime-timedelta-measure-time/
+
+        [キーボード打鍵数を1s間隔で検出する]
+        '''
+        # import win32gui as wg
+        # import win32process as wp
+        # import win32com.client as wcli
 
         try:
+            print("Screen Size: {}".format(pyautogui.size()))
+            sum_mouse_move_pxs = 0
+            dif_x, dif_y = 0, 0
+            recent_time = time.time()
+            recent_x, recent_y = pyautogui.position()
             while True:
-                
+                current_time = time.time()
+                x, y = pyautogui.position()
+                if x != recent_x or y != recent_y: # 直前のカーソル座標との違いがあったら
+                    dif_x = x - recent_x
+                    dif_y = y - recent_y
+                    sum_mouse_move_pxs += math.sqrt(dif_x * dif_x + dif_y*dif_y)
+                    recent_x, recent_y = x, y
 
-                time.sleep(1)
+                if current_time - recent_time > 1.0: # 1秒程度経ったら
+                    recent_time = current_time
+                    print(sum_mouse_move_pxs)
+                    sum_mouse_move_pixels = 0
+                
         except KeyboardInterrupt:
             print("Exit")
     elif "Darwin" in os:
+        '''
+        
+        '''
         from AppKit import NSWorkspace as nsw
         from Quartz import (
             CGWindowListCopyWindowInfo,
@@ -120,8 +153,25 @@ if __name__ == "__main__":
 
         try:
             while True:
+                print("Screen Size: {}".format(pyautogui.size()))
+            sum_mouse_move_pxs = 0
+            dif_x, dif_y = 0, 0
+            recent_time = time.time()
+            recent_x, recent_y = pyautogui.position()
+            while True:
+                current_time = time.time()
+                x, y = pyautogui.position()
+                if x != recent_x or y != recent_y: # 直前のカーソル座標との違いがあったら
+                    dif_x = x - recent_x
+                    dif_y = y - recent_y
+                    sum_mouse_move_pxs += math.sqrt(dif_x * dif_x + dif_y*dif_y)
+                    recent_x, recent_y = x, y
+
+                if current_time - recent_time > 1.0: # 1秒程度経ったら
+                    recent_time = current_time
+                    print(sum_mouse_move_pxs)
+                    sum_mouse_move_pixels = 0
                 
-                time.sleep(1)
         except KeyboardInterrupt:
             print("Exit")
 
