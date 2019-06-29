1a147f16bbd9c3b1c6c38e9b9c8d90c39e0db592
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-06-29 20:40:26 +0900

    Removed matplotlib and variable os

--- modules/Logger.py
+++ modules/Logger.py
@@ -5,16 +5,13 @@
 ●ガントチャート生成・出力
 ・ファイル名にタイムスタンプつけとく
 '''
-import matplotlib.pyplot as plt
 import numpy as np
 # import threading
 import concurrent.futures as confu
 from datetime import datetime # 定期間隔書き出しのタイムスタンプ用
 
 class Logger:
-    os = ""
-    def __init__(self, os, uuid=""):
-        self.os = os
+    def __init__(self, uuid=""):
         if uuid:
             self.uuid = uuid
         # データ受け取り・定期間隔でのファイル書き出しの並列処理なので最大2？
@@ -22,5 +19,3 @@ class Logger:
 
     def close(self): pass
 
-class PlotGraph:
-    def __init__(self): pass
