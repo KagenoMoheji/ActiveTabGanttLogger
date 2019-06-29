d25b2f8fc1eff8ebb2d0d9a2dbcd0e8113a83b61
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-06-29 20:42:05 +0900

    Created for plotter

---
+++ modules/Plotter.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Plotter:
+    def __init__(self):
+        '''
+        ●標準入力で
+        (1)出力モードの選択(filter_tab/set_interval)
+        (2)出力モードに合わせた必須項目への回答
+        ・fiter_tab -> 不要なタブ名(ログからコピペ)をまとめたテキストファイルの指定
+        ・set_interval -> デルタtの数値指定(単位は秒)
+        '''
+        pass
