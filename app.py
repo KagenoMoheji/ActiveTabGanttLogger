5ed460caf0acbc64ae7473fc3d6a84f26ea3277d
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-06-30 15:32:55 +0900

    Add arg 'uuid' to Plotter(), and trying getting type-count

--- app.py
+++ app.py
@@ -33,14 +33,14 @@ def main():
 
     # Start main process(thread-loop) in accordance with mode
     if mode == "Alone":
-        alone = Alone(os)
+        alone = Alone(os, uuid)
         alone.run()
     elif mode == "Observer":
         observer = Observer(os, uuid)
         observer.start()
     elif mode == "Logger":
         logger = Logger(uuid)
-        plotter = Plotter()
+        plotter = Plotter(uuid)
         logger.run()
         plotter.run()
     elif mode == "Plotter":
@@ -49,6 +49,14 @@ def main():
         plotter.run()
 
 
+
+# sum_keyboard_cnt = 0
+# def on_press(key):
+#     global sum_keyboard_cnt
+#     sum_keyboard_cnt += 1
+#     return False
+# def on_release(key):
+#     return False
 
 if __name__ == "__main__":
     '''
@@ -89,93 +97,64 @@ if __name__ == "__main__":
     import psutil
     import math
     # import pyautogui
-    from pynput.mouse import Controller as mctrl
+    # from pynput.mouse import Controller as mctrl
+    from pynput import keyboard
+    import threading
 
     os = platform.platform(terse=True)
-    mouse = mctrl()
     if "Windows" in os:
         '''
-        [マウスの移動ピクセル数を1s間隔で検出する]
-        ●リアルタイムで動作確認するとき，出力されたログをVSCodeで開いて
-        続けてアクションを起こすと更新を見ることができそう.
-        ●pyautoguiで実装する(Windows・Mac両対応)
-        ・https://qiita.com/hirohiro77/items/78e26a59c2e45a0fe4e3
-        ・https://pyautogui.readthedocs.io/en/latest/mouse.html#the-screen-and-mouse-position
-        ●pynputで実装する
-        ・https://pynput.readthedocs.io/en/latest/mouse.html
-        ●win32guiで実装する
-        ・https://stackoverflow.com/a/3698659
-        ●「1秒間隔」
-        ・unix時間の差分で求めるのが良さそう．
-        ・datetimeのmicrosecondsでやろうと思ったけど，1000000になる前に0にされるので差分計算できない
-        ・https://note.nkmk.me/python-datetime-timedelta-measure-time/
+        [キーボードの1sごとの打鍵数をカウントする]
+        ●pynputで実装
+        ・https://stackoverflow.com/a/45592445
+        ・https://pynput.readthedocs.io/en/latest/keyboard.html#monitoring-the-keyboard
+        ・上の2つ目のリンクはThread使用だったので適していないと思ったけど，1つ目のリンクで行けた
         '''
-        # import win32gui as wg
-        # import win32process as wp
-        # import win32com.client as wcli
-
         try:
-            sum_mouse_move_pxs = 0
-            dif_x, dif_y = 0, 0
             recent_time = time.time()
-            recent_x, recent_y = mouse.position
+            sum_keyboard_cnt = 0
+            on_press = lambda key: False
+            on_release = lambda key: False 
             while True:
                 current_time = time.time()
-                x, y = mouse.position
-                if x != recent_x or y != recent_y: # 直前のカーソル座標との違いがあったら
-                    dif_x = x - recent_x
-                    dif_y = y - recent_y
-                    sum_mouse_move_pxs += math.sqrt(dif_x * dif_x + dif_y*dif_y)
-                    recent_x, recent_y = x, y
+
+                # with keyboard.Listener(on_press=on_press, on_release=on_release) as listener: # global使う場合
+                #     listener.join()
+
+                with keyboard.Listener(on_press=on_press) as listener: # lmbda使って挟む場合
+                    listener.join()
+                sum_keyboard_cnt += 1
+                with keyboard.Listener(on_release=on_release) as listener:
+                    listener.join()
 
                 if current_time - recent_time > 1.0: # 1秒程度経ったら
                     recent_time = current_time
-                    print(sum_mouse_move_pxs)
-                    sum_mouse_move_pxs = 0
+                    print(sum_keyboard_cnt)
+                    sum_keyboard_cnt = 0
                 
         except KeyboardInterrupt:
             print("Exit")
     elif "Darwin" in os:
-        '''
-        [マウスの移動ピクセル数を1s間隔で検出する]
-        ●Windowsと同じくpyautoguiで可能
-        ●pyautoguiモジュール使用時のエラー回避例
-        ・https://teratail.com/questions/159780
-        ●pynputでMacでも使用可能
-        ●Quartzでやる場合？
-        ・https://stackoverflow.com/questions/281133/how-to-control-the-mouse-in-mac-using-python
-        '''
-        # from AppKit import NSWorkspace as nsw
-        # from Quartz import (
-        #     CGWindowListCopyWindowInfo,
-        #     kCGWindowListOptionOnScreenOnly,
-        #     kCGNullWindowID
-        # )
-
         try:
-            sum_mouse_move_pxs = 0
-            dif_x, dif_y = 0, 0
+            sum_keyboard_cnt = 0
             recent_time = time.time()
-            recent_x, recent_y = mouse.position
+            on_press = lambda key: False
+            on_release = lambda key: False 
             while True:
                 current_time = time.time()
-                x, y = mouse.position
-                if x != recent_x or y != recent_y: # 直前のカーソル座標との違いがあったら
-                    dif_x = x - recent_x
-                    dif_y = y - recent_y
-                    sum_mouse_move_pxs += math.sqrt(dif_x * dif_x + dif_y*dif_y)
-                    recent_x, recent_y = x, y
-
                 if current_time - recent_time > 1.0: # 1秒程度経ったら
                     recent_time = current_time
-                    print(sum_mouse_move_pxs)
-                    sum_mouse_move_pxs = 0
+                    print(sum_keyboard_cnt)
+                    sum_keyboard_cnt = 0
+
+                with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+                    listener.join()
+                sum_keyboard_cnt += 1
+                with keyboard.Listener(on_release=on_release) as listener:
+                    listener.join()
                 
         except KeyboardInterrupt:
             print("Exit")
-
-
-    
 
 
 
