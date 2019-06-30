7a0b27fc67fa38bbdf28d6cc7e3f63d722bb8a39
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-06-30 15:35:51 +0900

    Enable uuid required, and add making dir

--- modules/Logger.py
+++ modules/Logger.py
@@ -3,6 +3,7 @@
 ●jsonのid順にログ追記
 ●受信の場合はenv.jsonを読み込んで環境変数にする
 '''
+import os
 # import threading
 import concurrent.futures as confu
 from datetime import datetime # 定期間隔書き出しのタイムスタンプ用
@@ -26,6 +27,7 @@ class RawDataStore:
     }
     '''
     raw_tab_data = deque([["", "t", -1, "", "", ""]])
+    tab_id = 0
     '''
     {
         "uuid": "",
@@ -37,6 +39,7 @@ class RawDataStore:
     }
     '''
     raw_mouse_data = deque([["", "m", -1, "", "", ""]])
+    mouse_id = 0
     '''
     {
         "uuid": "",
@@ -48,12 +51,32 @@ class RawDataStore:
     }
     '''
     raw_keyboard_data = deque([["", "k", -1, "", "", ""]])
+    keyboard_id = 0
 
 
 class Logger:
-    def __init__(self, uuid=""):
-        if uuid:
-            self.uuid = uuid
+    uuid = ""
+    def __init__(self, uuid):
+        '''
+        
+        References:
+            https://stackoverflow.com/a/12517490
+        '''
+        self.uuid = uuid
+
+        # ログ・グラフの出力先フォルダ生成
+        filename = "{}/README.txt".format(uuid)
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, "w") as f:
+            startdate = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+            text = """\
+UUID       : {uuid}
+StartDate  : {startdate}
+Supervisor : 
+Target User: 
+""".format(uuid=uuid, startdate=startdate)
+            f.write(text)
+
         # データ受け取り・定期間隔でのファイル書き出しの並列処理なので最大2？
         pass
 
