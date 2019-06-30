e25906f93194ef1c89eb336000a35bf95eb6bd25
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-06-30 22:08:51 +0900

    Try on Mac

--- app.py
+++ app.py
@@ -144,22 +144,26 @@ if __name__ == "__main__":
             print("Exit")
     elif "Darwin" in os:
         try:
-            sum_keyboard_cnt = 0
             recent_time = time.time()
+            sum_keyboard_cnt = 0
             on_press = lambda key: False
             on_release = lambda key: False 
             while True:
                 current_time = time.time()
-                if current_time - recent_time > 1.0: # 1秒程度経ったら
-                    recent_time = current_time
-                    print(sum_keyboard_cnt)
-                    sum_keyboard_cnt = 0
 
-                with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+                # with keyboard.Listener(on_press=on_press, on_release=on_release) as listener: # global使う場合
+                #     listener.join()
+
+                with keyboard.Listener(on_press=on_press) as listener: # lmbda使って挟む場合
                     listener.join()
                 sum_keyboard_cnt += 1
                 with keyboard.Listener(on_release=on_release) as listener:
                     listener.join()
+
+                if current_time - recent_time > 1.0: # 1秒程度経ったら
+                    recent_time = current_time
+                    print(sum_keyboard_cnt)
+                    sum_keyboard_cnt = 0
                 
         except KeyboardInterrupt:
             print("Exit")
