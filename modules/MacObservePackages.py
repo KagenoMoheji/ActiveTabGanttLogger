a7a54f08f969b985c2f9a2be334a60da0ea48500
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-07-01 23:42:50 +0900

    Implement detecting active-tab

--- modules/MacObservePackages.py
+++ modules/MacObservePackages.py
@@ -1,4 +1,5 @@
-import datetime
+from datetime import datetime
+import time
 import psutil
 from AppKit import NSWorkspace as nsw
 from Quartz import (
@@ -13,7 +14,35 @@ class ActiveTabObserver:
     
     def run(self):
         recent_active_tab_text = "START!"
+        try:
+            while True:
+                fw = nsw.sharedWorkspace().activeApplication()
+                active_pid = fw["NSApplicationProcessIdentifier"]
+                active_name = fw["NSApplicationName"]
+                active_tab_text = ""
+                cg_windows = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID)
+                for cg_window in cg_windows:
+                    if active_name == cg_window["kCGWindowOwnerName"] and cg_window["kCGWindowName"]:
+                        active_tab_text = cg_window["kCGWindowName"]
+                        break
+                if recent_active_tab_text != active_tab_text.upper():
+                    switched_time = datetime.now().strftime("%H:%M:%S.%f")
+                    recent_active_tab_text = active_tab_text.upper()
 
+                    # このあたりでsum_key_boardで操作する
+                    # ログするとか？hand_data()
+                    print("ActiveTab[{time}]: {pid}: {active_name}({tab_text})".format(
+                        time=switched_time,
+                        pid=active_pid,
+                        active_name=active_name,
+                        tab_text=active_tab_text))
+                time.sleep(0.001)
+        except KeyboardInterrupt:
+            print("ActiveTabObserver.py: KeyboardInterrupt")
+
+    def hand_data(self):
+        # キューに格納するか，送信するかはここで
+        pass
     def close(self):
         pass
 
--- modules/WinObservePackages.py
+++ modules/WinObservePackages.py
@@ -1,8 +1,8 @@
-import datetime
+from datetime import datetime
+import time
 import psutil
 import win32gui as wg
 import win32process as wp
-import win32com.client as wcli
 
 class ActiveTabObserver:
     def __init__(self):
@@ -13,9 +13,33 @@ class ActiveTabObserver:
         try:
             while True:
                 fw = wg.GetForegroundWindow()
+                active_pid = wp.GetWindowThreadProcessId(fw)[-1]
+                # print("pids: {}".format(wp.GetWindowThreadProcessId(fw)))
+                active_name = psutil.Process(active_pid).name()
+                active_tab_text = wg.GetWindowText(fw)
+                if recent_active_tab_text != active_tab_text.upper():
+                    switched_time = datetime.now().strftime("%H:%M:%S.%f")
+                    recent_active_tab_text = active_tab_text.upper()
+                    if "CHROME" in active_name.upper():
+                        # In Windows10, we have to reshape the tab_text(title) when
+                        # active_name is Google Chrome.
+                        splitted_active_tab_text = active_tab_text.split(" - ")[:-1]
+                        active_tab_text = " - ".join(splitted_active_tab_text)
+
+                    # このあたりでsum_key_boardで操作する
+                    # ログするとか？hand_data()
+                    print("ActiveTab[{time}]: {pid}: {active_name}({tab_text})".format(
+                        time=switched_time,
+                        pid=active_pid,
+                        active_name=active_name,
+                        tab_text=active_tab_text))
+                # time.sleep(0.001)
         except KeyboardInterrupt:
             print("ActiveTabObserver.py: KeyboardInterrupt")
 
+    def hand_data(self):
+        # キューに格納するか，送信するかはここで
+        pass
     def close(self):
         pass
 
