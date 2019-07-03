7e1ab65caf9955ea93d273ec553569e587bf5dea
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-07-03 21:42:06 +0900

    Add importing modules, and a flag managing threads, and remove close()

--- modules/MacObservePackages.py
+++ modules/MacObservePackages.py
@@ -7,48 +7,47 @@ from Quartz import (
     kCGWindowListOptionOnScreenOnly,
     kCGNullWindowID
 )
+import modules.Global as global_v
 
 class ActiveTabObserver:
     def __init__(self):
         pass
     
     def run(self):
+        # try:
         recent_active_tab_text = "START!"
-        try:
-            while True:
-                try:
-                    fw = nsw.sharedWorkspace().activeApplication()
-                    active_pid = fw["NSApplicationProcessIdentifier"]
-                    active_name = fw["NSApplicationName"]
-                    active_tab_text = ""
-                    cg_windows = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID)
-                    for cg_window in cg_windows:
-                        if active_name == cg_window["kCGWindowOwnerName"] and cg_window["kCGWindowName"]:
-                            active_tab_text = cg_window["kCGWindowName"]
-                            break
-                except (ValueError, psutil.NoSuchProcess):
-                    # pid取得が間に合ってなかったら
-                    # print("Error: Failed in getting process information")
-                    continue
-                if recent_active_tab_text != active_tab_text.upper():
-                    switched_time = datetime.now().strftime("%H:%M:%S.%f")
-                    recent_active_tab_text = active_tab_text.upper()
+        while not global_v.is_switched_to_exit:
+            try:
+                fw = nsw.sharedWorkspace().activeApplication()
+                active_pid = fw["NSApplicationProcessIdentifier"]
+                active_name = fw["NSApplicationName"]
+                active_tab_text = ""
+                cg_windows = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID)
+                for cg_window in cg_windows:
+                    if active_name == cg_window["kCGWindowOwnerName"] and cg_window["kCGWindowName"]:
+                        active_tab_text = cg_window["kCGWindowName"]
+                        break
+            except (ValueError, psutil.NoSuchProcess):
+                # pid取得が間に合ってなかったら
+                # print("Error: Failed in getting process information")
+                continue
+            if recent_active_tab_text != active_tab_text.upper():
+                switched_time = datetime.now().strftime("%H:%M:%S.%f")
+                recent_active_tab_text = active_tab_text.upper()
 
-                    # このあたりでsum_key_boardで操作する
-                    # ログするとか？hand_data()
-                    print("ActiveTab[{time}]: {pid}: {active_name}({tab_text})".format(
-                        time=switched_time,
-                        pid=active_pid,
-                        active_name=active_name,
-                        tab_text=active_tab_text))
-                time.sleep(0.001)
-        except KeyboardInterrupt:
-            print("ActiveTabObserver.py: KeyboardInterrupt")
+                # このあたりでsum_key_boardで操作する
+                # ログするとか？hand_data()
+                print("ActiveTab[{time}]: {pid}: {active_name}({tab_text})".format(
+                    time=switched_time,
+                    pid=active_pid,
+                    active_name=active_name,
+                    tab_text=active_tab_text))
+            time.sleep(0.001)
+        # except KeyboardInterrupt:
+        #     print("ActiveTabObserver.py: KeyboardInterrupt")
 
     def hand_data(self):
         # キューに格納するか，送信するかはここで
-        pass
-    def close(self):
         pass
 
 
--- modules/WinObservePackages.py
+++ modules/WinObservePackages.py
@@ -3,48 +3,46 @@ import time
 import psutil
 import win32gui as wg
 import win32process as wp
+import modules.Global as global_v
 
 class ActiveTabObserver:
     def __init__(self):
         pass
 
     def run(self):
+        # try:
         recent_active_tab_text = "START!"
-        try:
-            while True:
-                try:
-                    fw = wg.GetForegroundWindow()
-                    active_pid = wp.GetWindowThreadProcessId(fw)[-1]
-                    # print("pids: {}".format(wp.GetWindowThreadProcessId(fw)))
-                    active_name = psutil.Process(active_pid).name()
-                    active_tab_text = wg.GetWindowText(fw)
-                except (ValueError, psutil.NoSuchProcess):
-                    # pid取得が間に合ってなかったら
-                    # print("Error: Failed in getting process information")
-                    continue
-                if recent_active_tab_text != active_tab_text.upper():
-                    switched_time = datetime.now().strftime("%H:%M:%S.%f")
-                    recent_active_tab_text = active_tab_text.upper()
-                    splitted_active_tab_text = active_tab_text.split(" - ")
-                    if len(splitted_active_tab_text) > 1:
-                        # Remove application name from tab text
-                        active_tab_text = " - ".join(splitted_active_tab_text[:-1])
+        while not global_v.is_switched_to_exit:
+            try:
+                fw = wg.GetForegroundWindow()
+                active_pid = wp.GetWindowThreadProcessId(fw)[-1]
+                active_name = psutil.Process(active_pid).name()
+                active_tab_text = wg.GetWindowText(fw)
+            except (ValueError, psutil.NoSuchProcess):
+                # pid取得が間に合ってなかったら
+                # print("Error: Failed in getting process information")
+                continue
+            if recent_active_tab_text != active_tab_text.upper():
+                switched_time = datetime.now().strftime("%H:%M:%S.%f")
+                recent_active_tab_text = active_tab_text.upper()
+                splitted_active_tab_text = active_tab_text.split(" - ")
+                if len(splitted_active_tab_text) > 1:
+                    # Remove application name from tab text
+                    active_tab_text = " - ".join(splitted_active_tab_text[:-1])
 
-                    # このあたりでsum_key_boardで操作する
-                    # ログするとか？hand_data()
-                    print("ActiveTab[{time}]: {pid}: {active_name}({tab_text})".format(
-                        time=switched_time,
-                        pid=active_pid,
-                        active_name=active_name,
-                        tab_text=active_tab_text))
-                time.sleep(0.001)
-        except KeyboardInterrupt:
-            print("ActiveTabObserver.py: KeyboardInterrupt")
+                # このあたりでsum_key_boardで操作する
+                # ログするとか？hand_data()
+                print("ActiveTab[{time}]: {pid}: {active_name}({tab_text})".format(
+                    time=switched_time,
+                    pid=active_pid,
+                    active_name=active_name,
+                    tab_text=active_tab_text))
+            time.sleep(0.001)
+        # except KeyboardInterrupt:
+        #     print("ActiveTabObserver.py: KeyboardInterrupt")
 
     def hand_data(self):
         # キューに格納するか，送信するかはここで
-        pass
-    def close(self):
         pass
 
 
