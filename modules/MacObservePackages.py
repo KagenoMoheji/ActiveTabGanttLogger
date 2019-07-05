28a2420b227bf873e2510b7c0bcef8299ddf16c1
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-07-06 04:52:45 +0900

    Check on Mac

--- modules/MacObservePackages.py
+++ modules/MacObservePackages.py
@@ -10,8 +10,14 @@ from Quartz import (
 import modules.Global as global_v
 
 class ActiveTabObserver:
-    def __init__(self):
-        pass
+    uuid = ""
+    data_process = None
+    def __init__(self, uuid, is_alone):
+        self.uuid = uuid
+        if is_alone:
+            self.data_process = self.enqueue_data
+        else:
+            self.data_process = self.send_json
     
     def run(self):
         # try:
@@ -19,7 +25,7 @@ class ActiveTabObserver:
         while not global_v.is_switched_to_exit:
             try:
                 fw = nsw.sharedWorkspace().activeApplication()
-                active_pid = fw["NSApplicationProcessIdentifier"]
+                # active_pid = fw["NSApplicationProcessIdentifier"]
                 active_name = fw["NSApplicationName"]
                 active_tab_text = ""
                 cg_windows = CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly, kCGNullWindowID)
@@ -34,21 +40,29 @@ class ActiveTabObserver:
             if recent_active_tab_text != active_tab_text.upper():
                 switched_time = datetime.now().strftime("%H:%M:%S.%f")
                 recent_active_tab_text = active_tab_text.upper()
-
-                # このあたりでsum_key_boardで操作する
-                # ログするとか？hand_data()
-                print("ActiveTab[{time}]: {pid}: {active_name}({tab_text})".format(
-                    time=switched_time,
-                    pid=active_pid,
-                    active_name=active_name,
-                    tab_text=active_tab_text))
+                self.data_process(switched_time, active_name, active_tab_text)
+                # print("ActiveTab[{time}]: {pid}: {active_name}({tab_text})".format(
+                #     time=switched_time,
+                #     pid=active_pid,
+                #     active_name=active_name,
+                #     tab_text=active_tab_text))
             time.sleep(0.001)
         # except KeyboardInterrupt:
         #     print("ActiveTabObserver.py: KeyboardInterrupt")
 
-    def hand_data(self):
-        # キューに格納するか，送信するかはここで
+    def send_json(self, t, active_name, tab_text):
         pass
+
+    def enqueue_data(self, t, active_name, tab_text):
+        global_v.tab_id += 1
+        global_v.tab_queue.append({
+            "uuid": self.uuid,
+            "type": "t",
+            "id": global_v.tab_id,
+            "activeName": active_name,
+            "tabText": tab_text,
+            "startTime": t
+        })
 
 
 
