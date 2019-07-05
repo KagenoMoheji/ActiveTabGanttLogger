813e3c5813d3235770e92f96dc0ae454129553b2
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-07-06 04:55:52 +0900

    Add an argument 'is_alone' for class 'ActiveTabObserver', and Implement functions 'enqueue_data'

--- modules/WinObservePackages.py
+++ modules/WinObservePackages.py
@@ -6,8 +6,14 @@ import win32process as wp
 import modules.Global as global_v
 
 class ActiveTabObserver:
-    def __init__(self):
-        pass
+    uuid = ""
+    data_process = None
+    def __init__(self, uuid, is_alone):
+        self.uuid = uuid
+        if is_alone:
+            self.data_process = self.enqueue_data
+        else:
+            self.data_process = self.send_json
 
     def run(self):
         # try:
@@ -30,20 +36,30 @@ class ActiveTabObserver:
                     # Remove application name from tab text
                     active_tab_text = " - ".join(splitted_active_tab_text[:-1])
 
-                # このあたりでsum_key_boardで操作する
-                # ログするとか？hand_data()
-                print("ActiveTab[{time}]: {pid}: {active_name}({tab_text})".format(
-                    time=switched_time,
-                    pid=active_pid,
-                    active_name=active_name,
-                    tab_text=active_tab_text))
+                self.data_process(switched_time, active_name, active_tab_text)
+                # print("ActiveTab[{time}]: {pid}: {active_name}({tab_text})".format(
+                #     time=switched_time,
+                #     pid=active_pid,
+                #     active_name=active_name,
+                #     tab_text=active_tab_text))
             time.sleep(0.001)
         # except KeyboardInterrupt:
         #     print("ActiveTabObserver.py: KeyboardInterrupt")
 
-    def hand_data(self):
-        # キューに格納するか，送信するかはここで
+    def send_json(self, t, active_name, tab_text):
         pass
+
+    def enqueue_data(self, t, active_name, tab_text):
+        global_v.tab_id += 1
+        global_v.tab_queue.append({
+            "uuid": self.uuid,
+            "type": "t",
+            "id": global_v.tab_id,
+            "activeName": active_name,
+            "tabText": tab_text,
+            "startTime": t
+        })
+
 
 
 '''
