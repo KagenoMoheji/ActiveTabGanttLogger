0d487f99d3fabecc7cdd5e82f8291d36bb514f92
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-07-06 04:52:29 +0900

    Implement a function 'output' for mode 'Alone'

--- modules/Logger.py
+++ modules/Logger.py
@@ -4,25 +4,31 @@
 ●受信の場合はenv.jsonを読み込んで環境変数にする
 '''
 import os
+import time
 from datetime import datetime
-# import numpy as np
+# import concurrent.futures as confu
 import modules.Global as global_v
+from modules.Public import StrFormatter
 
 class Logger:
     uuid = ""
+    strfmr = None
+    dirname = ""
     def __init__(self, uuid):
         '''
         
         References:
             https://stackoverflow.com/a/12517490
+            https://kokufu.blogspot.com/2018/04/python-with.html
         '''
         self.uuid = uuid
-
-        # ログ・グラフの出力先フォルダ生成
-        filename = "ganttlogger_logs/{}/README.txt".format(uuid)
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        with open(filename, "w") as f:
-            startdate = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        self.strfmr = StrFormatter()
+        # Create output dirs
+        self.dirname = "ganttlogger_logs/{}".format(uuid)
+        readme = "{}/README.txt".format(self.dirname)
+        os.makedirs(os.path.dirname(readme), exist_ok=True)
+        with open(readme, "w", encoding="utf-8") as f:
+            startdate = datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f")
             text = """\
 UUID       : {uuid}
 StartDate  : {startdate}
@@ -31,23 +37,67 @@ Target User:
 """.format(uuid=uuid, startdate=startdate)
             f.write(text)
 
-        # データ受け取り・定期間隔でのファイル書き出しの並列処理なので最大2？
-        pass
-
-    def run_alone(self):
-        print("Hello, Logger!")
-
     def run_logger(self):
+        # receive_json()とoutput()のスレッド展開
         print("Hello, Logger!")
 
     def receive_json(self):
-        # jsonデータを受信してRawDataに格納する
+        # jsonデータを受信してキューに格納する
         pass
 
-    def file_output(self):
-        # RawDataの先頭から取り出して(かつ削除して)ファイル書き出し
-        # ただしid=-1なら無視
-        pass
+    def output(self):
+        with open("{}/active_tab.csv".format(self.dirname), "a", encoding="utf-8") as ft,\
+            open("{}/active_mouse.csv".format(self.dirname), "a", encoding="utf-8") as fm,\
+            open("{}/active_keyboard.csv".format(self.dirname), "a", encoding="utf-8") as fk:
+            # Write attributes
+            ft.write("StartTime,ApplicationName,TabText\n")
+            fm.write("Time,MoveDistance\n")
+            fk.write("Time,PressCount\n")
+            # Add logs
+            is_all_empty = True
+            while True:
+                print("Hello!!!!!!")
+                len_tab_queue = len(global_v.tab_queue)
+                len_mouse_queue = len(global_v.mouse_queue)
+                len_keyboard_queue = len(global_v.keyboard_queue)
+                if len_tab_queue > 0:
+                    is_all_empty = False
+                    for _ in range(len_tab_queue):
+                        data = global_v.tab_queue.popleft()
+                        log = "{startTime},{activeName},{tabText}\n".format(
+                            startTime=data["startTime"],
+                            activeName=data["activeName"],
+                            tabText=data["tabText"]
+                        )
+                        ft.write(log)
+                if len_mouse_queue > 0:
+                    is_all_empty = False
+                    for _ in range(len_mouse_queue):
+                        data = global_v.mouse_queue.popleft()
+                        log = "{time},{dis}\n".format(
+                            time=data["time"],
+                            dis=data["distance"]
+                        )
+                        fm.write(log)
+                if len_keyboard_queue > 0:
+                    is_all_empty = False
+                    for _ in range(len_keyboard_queue):
+                        data = global_v.keyboard_queue.popleft()
+                        log = "{time},{cnt}\n".format(
+                            time=data["time"],
+                            cnt=data["count"]
+                        )
+                        fk.write(log)
+                if is_all_empty and global_v.is_switched_to_exit:
+                    # This is a signal that all logging finished and CLI will exit 
+                    print(self.strfmr.get_colored_console_log("yellow",
+                        "Logging maybe all finished"))
+                    break
+                elif is_all_empty:
+                    # This is a signal that logging is finishing
+                    print(self.strfmr.get_colored_console_log("yellow",
+                        "All queue are empty"))
+                is_all_empty = True
+                time.sleep(2) # 何秒でも良さそう
 
-    def close(self): pass
 
