f5fe959d79afef0a9e9c032aec64c1f4f28317c4
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-07-12 07:09:55 +0900

    Add code-commentout about exiting by thread-loop-exit-error

--- modules/Observer.py
+++ modules/Observer.py
@@ -45,6 +45,9 @@ class Observer:
             self.executor.submit(self.ob_keyboard.run)
             while not global_v.is_switched_to_exit:
                 time.sleep(1)
+            # if global_v.is_threadloop_error: # When "Thread loop exited by any problem!!!!" occured
+            #     self.executor.shutdown()
+            #     exit()
             is_confirmed_exiting = self.confirm_exiting()
             if is_confirmed_exiting:
                 print(self.strfmr.get_colored_console_log("yellow",
