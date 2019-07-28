bc3b0225746962f3e45a8862579393fed14522a9
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2019-07-28 14:26:27 +0900

    Implement a part of Merger.py

--- exe/modules/Merger.py
+++ exe/modules/Merger.py
@@ -5,6 +5,7 @@ import datetime
 from modules.Public import StrFormatter
 
 class Merger:
+    currdir = ""
     mergedir = ""
     run_merge = {
         "active_tab": False,
@@ -15,18 +16,18 @@ class Merger:
     def __init__(self):
         self.strfmr = StrFormatter()
         # Check whether current folder name is "ganttlogger_logs"
-        currdir = os.getcwd()
+        self.currdir = os.getcwd()
         is_win = "Windows" in platform.platform(terse=True)
         curr_name = ""
         if  is_win:
-            curr_name = currdir.split("\\")[-1]
+            curr_name = self.currdir.split("\\")[-1]
         else:
-            curr_name = currdir.split("/")[-1]
+            curr_name = self.currdir.split("/")[-1]
         if curr_name != "ganttlogger_logs":
             print(self.strfmr.get_colored_console_log("red",
                 "Error: You must move to a folder 'ganttlogger_logs'."))
             sys.exit()
-        self.mergedir = "{currdir}/merged_{datetime}/".format(currdir=currdir, datetime=datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
+        self.mergedir = "{currdir}/merged_{datetime}/".format(currdir=self.currdir, datetime=datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
     
     def start(self):
         try:
@@ -67,24 +68,62 @@ class Merger:
 
             # Create new folder where is outputted merged logs
             os.makedirs(os.path.dirname(self.mergedir), exist_ok=True)
+            print("Created an output folder '{}'.".format(self.mergedir))
             self.run()
         except KeyboardInterrupt:
             print("Exit")
             sys.exit()
     
     def run(self):
-        # ここでganttlogger_logs内のID名のフォルダをREADMEの開始日時を元にソートしてファイル名リストに格納
+        # Get dictionary of directorys in a folder "ganttlogger_logs" except for directorys including "merged" in its name.
+        log_folders = {f: None for f in os.listdir(self.currdir) if (os.path.isdir(os.path.join(self.currdir, f))) and (not "merged" in f)}
+        # 
+        remove_keys_list = []
+        for key in log_folders.keys():
+            readme = "{dir}/{folder}/README.txt".format(dir=self.currdir, folder=key)
+            if not os.path.exists(readme):
+                remove_keys_list.append(key)
+                continue
+            # Read from text file until appearing 'StartDate' till 4 rows.
+            has_startdate = False
+            row_startdate = ""
+            with open(readme, "r", encoding="utf-8") as f:
+                for row in range(4):
+                    row_startdate = f.readline()
+                    if "StartDate" in row_startdate:
+                        has_startdate = True
+                        break
+            if not has_startdate: # If README.txt doesn't have a row "StartDate".
+                print(self.strfmr.get_colored_console_log("yellow",
+                    "Warning: File '{readme}' doesn't have a row 'StartDate'.".format(readme=readme)))
+                remove_keys_list.append(key)
+                continue
+            # Add value of "StartDate" to list
+            try:
+                log_folders[key] = datetime.datetime.strptime((row_startdate.split(": ")[-1]).strip(), "%Y/%m/%d %H:%M:%S.%f")
+            except ValueError:
+                print(self.strfmr.get_colored_console_log("red",
+                    "Error: Invalid format of a value of 'StartDate' in {readme}.".format(readme=readme)))
+                sys.exit()
+        # Remove values in specific keys in "log_folders"
+        for k in remove_keys_list:
+            log_folders.pop(k)
+        # Sort "log_folders" by datetime of values in ASC
+        log_folders = sorted(log_folders.items(), key=lambda x:x[1])
+#         print("""
+# log_folders: {log_folders}
+# """.format(log_folders=log_folders))
 
         if self.run_merge["active_tab"]:
-            self.merge_active_tab_logs()
+            self.merge_active_tab_logs(log_folders)
         if self.run_merge["mouse"]:
-            self.merge_mouse_logs()
+            self.merge_mouse_logs(log_folders)
         if self.run_merge["keyboard"]:
-            self.merge_keyboard_logs()
+            self.merge_keyboard_logs(log_folders)
 
-    def merge_active_tab_logs(self, folders_list):
+    def merge_active_tab_logs(self, sorted_folders_dict):
         print("ActiveTab merged!")
-    def merge_mouse_logs(self, folders_list):
+    def merge_mouse_logs(self, sorted_folders_dict):
         print("Mouse merged!")
-    def merge_keyboard_logs(self, folders_list):
+    def merge_keyboard_logs(self, sorted_folders_dict):
         print("Keyboard merged!")
